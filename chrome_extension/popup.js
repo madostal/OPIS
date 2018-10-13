0c6fadf53205dad3b9d81b84912db04a434a0012
lexajosef <34240968+lexajosef@users.noreply.github.com>
2018-10-13 15:16:06 +0200

    Initial commit
    Chrome extension for checking the basics of accesibility of HTML page.

---
+++ chrome_extension/popup.js
@@ -0,0 +1,19 @@
+let changeColor = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color', function(data) {
+	changeColor.style.backgroundColor = data.color;
+	changeColor.setAttribute('value', data.color);
+});
+
+changeColor.onclick = function() {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.executeScript(
+			tabs[0].id,
+			{ file: 'content-script.js' }, 
+			function(results) {
+				// TODO: later maybe showing results here in popup
+				document.getElementById('forResultText').innerHTML = results;
+			}
+		);
+	});
+};
